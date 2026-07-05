fd85a6c34b0d0cc86d0ec594e542f4126b300902
ezgigulkaraca <ezgigulkaraca@gmail.com>
2026-07-05 12:48:40 +0300

    Update file_reader.py

--- utils/file_reader.py
+++ utils/file_reader.py
@@ -1,1 +1,7 @@
+def read_file(file_path):
+    """
+    Verilen dosya yolundaki metni okur ve döndürür.
+    """
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
 
